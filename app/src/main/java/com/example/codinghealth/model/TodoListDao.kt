088a965ec54a2d67a418cf86204760605a35537c
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-03-24 19:48:25 +0900

    feat: 비동기 처리를 위한 Flow<List<*>> 추가, List 추가를 위한 insertAll 추가

--- app/src/main/java/com/example/codinghealth/model/TodoListDao.kt
+++ app/src/main/java/com/example/codinghealth/model/TodoListDao.kt
@@ -2,13 +2,18 @@ package com.example.codinghealth.model
 
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TodoListDao {
     @Query("SELECT * FROM todolist")
-    fun getAll(): List<TodoList>
+    fun getAll(): Flow<List<TodoList>>
+
+    @Insert
+    fun insertAll(vararg lists: TodoList)
 
     @Delete
     fun delete(todoList: TodoList)
