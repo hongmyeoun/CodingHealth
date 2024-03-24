944fc3bbe575b1e63982335dd441b6b14973211d
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-03-24 22:35:29 +0900

    feat: viewModel 파라미터를 추가를 위해 Main에서 넣어줌

--- app/src/main/java/com/example/codinghealth/MainActivity.kt
+++ app/src/main/java/com/example/codinghealth/MainActivity.kt
@@ -3,26 +3,26 @@ package com.example.codinghealth
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.codinghealth.model.data.AppDatabase
+import com.example.codinghealth.model.TodoListRepository
 import com.example.codinghealth.ui.theme.CodingHealthTheme
 import com.example.codinghealth.view.TodoList
+import com.example.codinghealth.viewModel.TodoViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val context = this // 또는 applicationContext를 사용할 수 있습니다.
+        val db = AppDatabase.getDatabase(context)
+        val todoListDao = db.todoListDao()
+        val repository = TodoListRepository(todoListDao)
+        val viewModel = TodoViewModel(repository)
+
         setContent {
             CodingHealthTheme {
-                TodoList()
+                TodoList(viewModel)
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CodingHealthTheme {
-        TodoList()
     }
 }
