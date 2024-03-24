cb3083c36079638ca00410911a83d96b208fa8be
DESKTOP-1SJ24AG\hongjin <hong80714@gmail.com>
2024-03-24 22:41:26 +0900

    feat: 전체를 관리하는 TodoViewModel, TodoList하나하나 관리하는 TodoItemViewModel 생성
    TodoViewModel
    - 파라미터로 repository를 받아옴
    - LazyColumn에 사용할 todoList
    - 바뀐 내용을 업데이트하는 함수 추가
        - update, textUpdate, checkUpdate
    - addList : 새 항목 추가
    - delete : 해당 항목 삭제
    
    TodoItemViewModel
    - textValueChange : textField 값 변경
    - checkboxValueChange : 체크박스 값 변경

--- app/src/main/java/com/example/codinghealth/viewModel/TodoViewModel.kt
+++ app/src/main/java/com/example/codinghealth/viewModel/TodoViewModel.kt
@@ -1,16 +1,60 @@
 package com.example.codinghealth.viewModel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.codinghealth.model.data.TodoList
+import com.example.codinghealth.model.TodoListRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
-class TodoViewModel: ViewModel() {
-    val listCount = mutableStateOf(0)
+class TodoViewModel(private val repository: TodoListRepository) : ViewModel() {
+    val todoList: StateFlow<List<TodoList>> = repository.getAll().stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 
-    fun addList(){
-        ++listCount.value
+    fun textUpdate(todoList: TodoList, text: String) {
+        val update = todoList.copy(script = text)
+        update(update)
     }
 
-    fun deleteList(){
-        --listCount.value
+    fun checkUpdate(todoList: TodoList, checked: Boolean) {
+        val update = todoList.copy(checked = checked)
+        update(update)
+    }
+
+    fun addList() {
+        val newTodoList = TodoList()
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insert(newTodoList)
+        }
+    }
+
+    private fun update(todoList: TodoList) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.update(todoList)
+        }
+    }
+
+    fun delete(todoList: TodoList) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.delete(todoList)
+        }
+    }
+
+}
+
+class TodoItemViewModel(initText: String, initChecked: Boolean) : ViewModel() {
+    var text by mutableStateOf(initText)
+    fun textValueChange(newText: String) {
+        text = newText
+    }
+
+    var isChecked by mutableStateOf(initChecked)
+    fun checkboxValueChange(newBoolean: Boolean){
+        isChecked = newBoolean
     }
 }
